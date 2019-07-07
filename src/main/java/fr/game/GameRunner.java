0f2d7643fb781b975d7406c757b2350757197b61
Ammach Mohamed Amine <maammach@norsys.fr>
2019-07-07 20:20:34 +0100

    chore: Add runner with its test file

---
+++ src/main/java/fr/game/GameRunner.java
@@ -0,0 +1,10 @@
+package fr.game;
+
+public class GameRunner {
+
+
+    public static void main(String args[]){
+    }
+
+
+}
---
+++ src/test/java/fr/game/GameRunnerTest.java
@@ -0,0 +1,10 @@
+package fr.game;
+
+import org.junit.Test;
+
+public class GameRunnerTest {
+
+    @Test
+    public void testMain() {
+    }
+}
